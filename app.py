4e587db6d481bf9659d5031c8cde67cc733238f9
fraser-health-project <myreen.randhawa@gmail.com>
2026-08-12 10:29:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ st.title("Fraser Health Needs Index")
 df = pd.read_csv("data/final_df.csv")
 st.dataframe(df)
 
+st.set_page_config(page_title="Fraser Health Needs Index", layout="wide")
+
+st.sidebar.title("Fraser Health Needs Index")
+page = st.sidebar.radio(
+    "Navigate",
+    ["Overview", "View By Map", "", "Clusters", "Methodology"]
+)
+
+st.title("Fraser Health Needs Index")
+st.markdown("An interactive tool ranking and grouping hospital systems' need across the Fraser Health Region.")
