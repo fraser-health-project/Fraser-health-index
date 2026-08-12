6e06a27c82c3f6ecfaa883cabbed9e705a2e8580
fraser-health-project <myreen.randhawa@gmail.com>
2026-08-12 11:03:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ for _, row in ranking.iterrows():
         textwrap.dedent(card),
         unsafe_allow_html=True
     )
-   fig = px.bar(
+    fig = px.bar(
         final_df.sort_values('Need Index', ascending=False),
         x='Need Index', y='Municipality', color='Cluster_Label',
         title="Need Index by Municipality"
