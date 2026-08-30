f2a1ba3bcbbd937397998a1eeaa308b4bc088fd5
fraser-health-project <myreen.randhawa@gmail.com>
2026-08-29 19:43:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -663,7 +663,7 @@ if page == "Overview":
             color_discrete_map=cluster_colors,
             title="Municipality Ranking by Need",
             custom_data=["Municipality", "Cluster_Label", "Group_Wrapped"])
-        fig_bar.update_layout(yaxis=dict(autorange='reversed', automargin=True),height=1000,)
+        fig_bar.update_layout(yaxis=dict(autorange='reversed', automargin=True),height=700,legend=dict(orientation="h",yanchor="bottom",y=-0.3,xanchor="center",x=0.5,),)
         st.markdown("Tap on a municipality for details")
         if st.button("Compare Municipalities"):
             st.session_state.view = "compare"
